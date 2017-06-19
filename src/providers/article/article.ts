01b0c3f58eed2cea29fb08085bff103fa4c17874
JARVIS-VOVA <jarvis.vova@gmail.com>
2017-06-19 11:41:58 +0300

    minor

--- src/providers/article/article.ts
+++ src/providers/article/article.ts
@@ -1,5 +1,5 @@
 import { Injectable }     from '@angular/core';
-import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
