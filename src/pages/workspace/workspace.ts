8b659bdf6963c3aeee832b260afa38bff64ccd45
JARVIS-VOVA <jarvis.vova@gmail.com>
2017-07-07 00:13:02 +0300

    Report: total, cost, revenue

--- src/pages/workspace/workspace.ts
+++ src/pages/workspace/workspace.ts
@@ -11,7 +11,6 @@ import { StorageProvider }    from '../../providers/storage/storage';
   templateUrl: 'workspace.html',
 })
 export class WorkspacePage {
-  public foundWorkspaces = [];
   public workspaceData = {
     currentId: false,
     currentTitle: false,
@@ -44,12 +43,47 @@ export class WorkspacePage {
     this.workspaceData.currentId = navParams.get('currentWorkspace');
     this.workspaceData.currentTitle = navParams.get('currentWorkspaceTitle');
     this.workspaceData.foundWorkspaces = navParams.get('foundWorkspaces');
+
+    if (!this.workspaceData.foundWorkspaces) {
+      this.storage.init().then((value)=>{
+        this.getWorkspaces()
+      });
+    }
   }
+
+  checkCurrentWorkspaceExistenz() {
+    let currentWorkspace = this.storage.getCurrentWorkspace()
+
+    if (currentWorkspace) {
+      if (this.workspaceData.currentId != currentWorkspace) {
+        this.workspaceData.currentId = currentWorkspace
+      }
+
+      if (this.workspaceData.foundWorkspaces) {
+        // перевіряю на існування курент id
+        let bool = false
+        for (var i = this.workspaceData.foundWorkspaces.length - 1; i >= 0; i--) {
+          if (this.workspaceData.foundWorkspaces[i].id === this.workspaceData.currentId) {
+            this.workspaceData.currentTitle = this.workspaceData.foundWorkspaces[i].title
+            bool = true
+            break
+          }
+        }
+
+        if (!bool) {
+          this.workspaceData.currentTitle = false
+          this.workspaceData.currentId = false
+          this.storage.deleteCurrentWorkspace()
+        }
+      }
+    }
+  };
 
   getWorkspaces() {
     this.workspace.getWorkspaces().subscribe(
       res => {
         this.workspaceData.foundWorkspaces = res;
+        this.checkCurrentWorkspaceExistenz()
       },
       error => {
         console.log("error", error)
